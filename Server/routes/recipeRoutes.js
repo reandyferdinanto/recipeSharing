bcb2a7c77b8b06ede55d2903eba4c9cdbd381fd6
reandyferdinanto <reandyferdinanto@gmail.com>
2022-11-12 05:50:23 +0700

    update routes for API

--- Server/routes/recipeRoutes.js
+++ Server/routes/recipeRoutes.js
@@ -7,6 +7,7 @@ router.get("/categories/:id", recipeController.exploreCategoriesById);
 router.post("/categories", recipeController.submitCategories);
 router.get("/recipes", recipeController.recipes);
 router.get("/recipes/:id", recipeController.exploreRecipesById);
-// router.get("/category", recipeController.submitCategories);
+router.post("/recipes", recipeController.submitRecipe);
+router.delete("/recipes/:id", recipeController.deleteRecipesById);
 
 module.exports = router;
