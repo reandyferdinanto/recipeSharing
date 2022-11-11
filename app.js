2a3cb073b226c8ec7f097d31e57ae1de9b3c0cbc
reandyferdinanto <reandyferdinanto@gmail.com>
2022-11-12 05:45:17 +0700

    update middleware

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const express = require("express");
-// const expressLayouts = require("express-ejs-layouts");
 const fileUpload = require("express-fileupload");
-// const session = require("express-session");
-// const cookieParser = require("cookie-parser");
-// const flash = require("connect-flash");
 const routes = require("./server/routes/recipeRoutes");
 const pools = require("./Server/models/database");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 require("dotenv").config();
 
@@ -28,5 +24,3 @@ pools.getConnection((err) => {
   if (!err) console.log("DB Connection succeded");
   else console.log("DB Connection failed ");
 });
-
-// app.listen(port, () => console.log(`Listening to port ${port}`));
