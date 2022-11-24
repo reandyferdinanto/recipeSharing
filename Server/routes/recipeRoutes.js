eae87ebfdd338613bad037bab9f10ab6309673d2
reandyferdinanto <reandyferdinanto@gmail.com>
2022-11-24 16:05:29 +0700

    Temporary for Trial

--- Server/routes/recipeRoutes.js
+++ Server/routes/recipeRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const recipeController = require("../controllers/recipeController");
 
 router.get("/categories", recipeController.categories);
-router.get("/categories/:id", recipeController.exploreCategoriesById);
-router.post("/categories", recipeController.submitCategories);
+// router.get("/categories/:id", recipeController.exploreCategoriesById);
+// router.post("/categories", recipeController.submitCategories);
 router.get("/recipes", recipeController.recipes);
-router.get("/recipes/:id", recipeController.exploreRecipesById);
-router.post("/recipes", recipeController.submitRecipe);
-router.delete("/recipes/:id", recipeController.deleteRecipesById);
+// router.get("/recipes/:id", recipeController.exploreRecipesById);
+// router.post("/recipes", recipeController.submitRecipe);
+// router.delete("/recipes/:id", recipeController.deleteRecipesById);
 
 module.exports = router;
