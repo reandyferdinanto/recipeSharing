e57f13f4fc65ddd85374fd0d36091b28a242689f
reandyferdinanto <reandyferdinanto@gmail.com>
2022-11-24 16:06:41 +0700

    Temporary for Trial, Still Updating Code

--- Server/controllers/recipeController.js
+++ Server/controllers/recipeController.js
@@ -1,131 +1,152 @@
-// const pools = require("./Server/models/database");
-
-// const { getConnection } = require("../models/database");
-const pool = require("../models/database");
-
-// # GET /
-// # Homepage
-
-// exports.homepage = async (req, res) => {
-//   res.render("index", { title: "Recipe Sharing" });
-// };
+const connection = require("../models/database");
 
 // using try catch
 
+// exports.categories = async (req, res) => {
+//   try {
+//     const limitNumber = 5;
+//     const categories = await connection
+//       .promise()
+//       .query("SELECT * FROM recipecategories", (err, rows) => {
+//         res.send(rows);
+//       });
+//   } catch (error) {
+//     res.status(500).send({ message: error.message || "Error Occured" });
+//   }
+// };
+
+//Coba coba
 exports.categories = async (req, res) => {
   try {
-    const limitNumber = 5;
-    const categories = await pool.query(
-      "SELECT * FROM recipesharing.recipecategories",
-      (err, rows) => {
-        res.send(rows);
-      }
-    );
-  } catch (error) {
-    res.status(500).send({ message: error.message || "Error Occured" });
-  }
-};
-
-exports.exploreCategoriesById = async (req, res) => {
-  try {
-    const categories = await pool.query(
-      `SELECT * FROM recipesharing.recipecategories WHERE id = ?`,
-      [req.params.id],
-      (err, rows) => {
-        res.send(rows);
-      }
-    );
-  } catch (error) {
-    res.status(500).send({ message: error.message || "Error Occured" });
-  }
-};
-
-exports.submitCategories = async (req, res) => {
-  //const params = req.body;
-  //console.log(req.body);
-  // console.log(req.body.categoryName);
-  // console.log(req.body.createdAt);
-  try {
-    const categories = await pool.query(
-      `INSERT INTO recipesharing.recipecategories (categoryName) VALUES (?)`,
-      Object.values(req.body),
-      (err, rows) => {
-        if (!err) {
-          res.send("category successfully created");
-        } else {
-          console.log(err);
-          console.log(categories);
-        }
-      }
-    );
-  } catch (error) {
-    res.status(500).send({ message: error.message || "Error Occured" });
+    const queryProducts = "SELECT * FROM recipecategories";
+    const users = await connection
+      .promise()
+      .query(queryProducts)
+      .then(([rows]) => {
+        res.status(200).send(rows);
+      });
+  } catch (err) {
+    res.json({
+      message: "Please Verify your Account",
+    });
   }
 };
 
 exports.recipes = async (req, res) => {
   try {
-    // const limitNumber = 20;
-    const categories = await pool.query(
-      "SELECT * FROM recipesharing.recipestable",
-      (err, rows) => {
+    const queryProducts = "SELECT * FROM recipestable";
+    const users = await connection
+      .promise()
+      .query(queryProducts)
+      .then(([rows]) => {
         res.status(200).send(rows);
-      }
-    );
-  } catch (error) {
-    res.status(500).send({ message: error.message || "Error Occured" });
+      });
+  } catch (err) {
+    res.json({
+      message: "Please Verify your Account",
+    });
   }
 };
+//--------------------------------------------------------------
 
-exports.exploreRecipesById = async (req, res) => {
-  try {
-    const categories = await pool.query(
-      `SELECT * FROM recipesharing.recipestable WHERE recipeID = ?`,
-      [req.params.id],
-      (err, rows) => {
-        res.send(rows);
-      }
-    );
-  } catch (error) {
-    res.status(500).send({ message: error.message || "Error Occured" });
-  }
-};
+// exports.exploreCategoriesById = async (req, res) => {
+//   try {
+//     const categories = await pool.query(
+//       `SELECT * FROM recipesharing.recipecategories WHERE id = ?`,
+//       [req.params.id],
+//       (err, rows) => {
+//         res.send(rows);
+//       }
+//     );
+//   } catch (error) {
+//     res.status(500).send({ message: error.message || "Error Occured" });
+//   }
+// };
 
-exports.submitRecipe = async (req, res) => {
-  //const params = req.body;
-  //console.log(req.body);
-  // console.log(req.body.categoryName);
-  // console.log(req.body.createdAt);
-  try {
-    const recipe = await pool.query(
-      `INSERT INTO recipesharing.recipestable (categoryName, recipeTitle, recipeIngredients, recipeDescription, recipeImages ) VALUES (?,?,?,?,?)`,
-      Object.values(req.body),
-      (err, rows) => {
-        if (!err) {
-          res.status(200).send({ message: "recipe successfully submitted" });
-        } else {
-          console.log(err);
-        }
-      }
-    );
-  } catch (error) {
-    res.status(500).send({ message: error.message || "Error Occured" });
-  }
-};
+// exports.submitCategories = async (req, res) => {
+//   //const params = req.body;
+//   //console.log(req.body);
+//   // console.log(req.body.categoryName);
+//   // console.log(req.body.createdAt);
+//   try {
+//     const categories = await pool.query(
+//       `INSERT INTO recipesharing.recipecategories (categoryName) VALUES (?)`,
+//       Object.values(req.body),
+//       (err, rows) => {
+//         if (!err) {
+//           res.send("category successfully created");
+//         } else {
+//           console.log(err);
+//           console.log(categories);
+//         }
+//       }
+//     );
+//   } catch (error) {
+//     res.status(500).send({ message: error.message || "Error Occured" });
+//   }
+// };
 
-exports.deleteRecipesById = async (req, res) => {
-  try {
-    const categories = await pool.query(
-      `DELETE FROM recipesharing.recipestable WHERE recipeID = ?`,
-      [req.params.id],
-      (err, rows) => {
-        res.status(200).send({ message: "Recipes successfully deleted" });
-      }
-    );
-  } catch (error) {
-    res.status(500).send({ message: error.message || "Error Occured" });
-  }
-};
+// exports.recipes = async (req, res) => {
+//   try {
+//     // const limitNumber = 20;
+//     const categories = await pool.query(
+//       "SELECT * FROM recipesharing.recipestable",
+//       (err, rows) => {
+//         res.status(200).send(rows);
+//       }
+//     );
+//   } catch (error) {
+//     res.status(500).send({ message: error.message || "Error Occured" });
+//   }
+// };
+
+// exports.exploreRecipesById = async (req, res) => {
+//   try {
+//     const categories = await pool.query(
+//       `SELECT * FROM recipesharing.recipestable WHERE recipeID = ?`,
+//       [req.params.id],
+//       (err, rows) => {
+//         res.send(rows);
+//       }
+//     );
+//   } catch (error) {
+//     res.status(500).send({ message: error.message || "Error Occured" });
+//   }
+// };
+
+// exports.submitRecipe = async (req, res) => {
+//   try {
+//     const recipe = await pool.query(
+//       `INSERT INTO recipesharing.recipestable (categoryName, recipeTitle, recipeIngredients, recipeDescription, recipeImages ) VALUES (?,?,?,?,?)`,
+//       Object.values(req.body),
+//       (err, rows) => {
+//         if (!err) {
+//           res.status(200).send({ message: "recipe successfully submitted" });
+//         } else {
+//           console.log(err);
+//         }
+//       }
+//     );
+//   } catch (error) {
+//     res.status(500).send({ message: error.message || "Error Occured" });
+//   }
+// };
+
+// exports.deleteRecipesById = async (req, res) => {
+//   try {
+//     const categories = await pool.query(
+//       `DELETE FROM recipesharing.recipestable WHERE recipeID = ?`,
+//       [req.params.id],
+//       (err, rows) => {
+//         res.status(200).send({ message: "Recipes successfully deleted" });
+//       }
+//     );
+//   } catch (error) {
+//     res.status(500).send({ message: error.message || "Error Occured" });
+//   }
+// };
+
+//---------------------------------------------------------------------
 
 // exports.submitCategories = async (req, res) => {
 //   try {
